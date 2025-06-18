7623360b719d26f8cff9309db4da6ec15eb5949e
Sansan99999 <91546401+Sansan99999@users.noreply.github.com>
2025-06-19 00:16:49 +0700

    Update game.js

--- scripts/game.js
+++ scripts/game.js
@@ -21,9 +21,11 @@ const config = {
 
 let player;
 let cursors;
+let bullets;
 let enemies;
 let score = 0;
 let scoreText;
+let spacebar;
 
 const game = new Phaser.Game(config);
 
@@ -31,6 +33,7 @@ function preload() {
     // Load assets
     this.load.image('player', 'https://upload.wikimedia.org/wikipedia/commons/3/3e/Pixel_circle.svg');
     this.load.image('enemy', 'https://upload.wikimedia.org/wikipedia/commons/5/55/Red_circle.svg');
+    this.load.image('bullet', 'https://upload.wikimedia.org/wikipedia/commons/f/fd/Blue_circle.svg');
 }
 
 function create() {
@@ -40,10 +43,17 @@ function create() {
 
     // Input keyboard
     cursors = this.input.keyboard.createCursorKeys();
+    spacebar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
 
     // Buat musuh
     enemies = this.physics.add.group();
     spawnEnemy(this);
+
+    // Buat peluru
+    bullets = this.physics.add.group();
+
+    // Collider: Bullet vs Enemy
+    this.physics.add.overlap(bullets, enemies, hitEnemy, null, this);
 
     // Tampilkan skor
     scoreText = this.add.text(10, 10, 'Score: 0', { fontSize: '20px', fill: '#fff' });
@@ -58,6 +68,18 @@ function update() {
     } else {
         player.setVelocityX(0);
     }
+
+    // Tembak peluru
+    if (Phaser.Input.Keyboard.JustDown(spacebar)) {
+        fireBullet(this);
+    }
+
+    // Hapus peluru yang keluar layar
+    bullets.children.iterate(function (bullet) {
+        if (bullet && bullet.y < 0) {
+            bullet.destroy();
+        }
+    });
 }
 
 // Fungsi spawn musuh
@@ -72,4 +94,20 @@ function spawnEnemy(scene) {
         callbackScope: scene,
         loop: true
     });
-                                     }
+}
+
+// Fungsi tembak peluru
+function fireBullet(scene) {
+    let bullet = bullets.create(player.x, player.y - 20, 'bullet');
+    bullet.setVelocityY(-300);
+}
+
+// Fungsi ketika peluru kena musuh
+function hitEnemy(bullet, enemy) {
+    bullet.destroy();
+    enemy.destroy();
+
+    // Tambah skor
+    score += 10;
+    scoreText.setText('Score: ' + score);
+    }
